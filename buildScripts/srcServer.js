1ff98cdc7037655cb035574bfacde3e20f7048bb
Sumit Saha <ssah27@sapient.com>
2017-08-13 17:18:39 +0530

    Checking In Initial Set Up install

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -2,7 +2,7 @@ var express = require('express');
 var path = require('path');
 var open = require('open');
 
-var port = 8888;
+var port = 3000;
 
 var app = express();
 
@@ -12,7 +12,7 @@ app.get('/', function (req, res) {
 
 app.listen(port, function (err) {
     if (err) {
-        console.log(err)
+
     } else {
         open('http://localhost:' + port);
     }
